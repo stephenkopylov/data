5f2649daf16aee0a5e6046fa26f2fe269bbaca30
Stepan Kopylov <stepan.kopylov@ibitcy.com>
2018-09-13 18:59:49 +0300

    Some fixes

--- src/FetchManager.ts
+++ src/FetchManager.ts
@@ -12,84 +12,90 @@ export class FetchManager {
     }
 
     public static testFetchNew() {
+
+        let period: any = [
+            {
+                "id": 6,
+                "value": "q1",
+                "operator": "eq"
+            },
+            {
+                "id": 7,
+                "value": 2015,
+                "operator": "eq"
+            }
+        ];
+
         axios.post(
             'https://simfin.com/api/v1/finder?api-key=uwAPKLPaWmwjxnYA8nwmsC7tm6zQsswO',
             JSON.stringify({
+                "resultsPerPage": 300,
                 "search": [
-                    {
-                        "indicatorId": "0-73",
-                        "meta": [
-                            {
-                                "value": "Diversified Holdings",
-                                "operator": "eq"
-                            }
-                        ],
-                        "condition": {
-                            "value": "Diversified Holdings",
-                            "operator": "eq"
-                        }
-                    },
+                    // {
+                    //     "indicatorId": "0-73",
+                    //     "meta": [
+                    //         {
+                    //             "value": "Diversified Holdings",
+                    //             "operator": "eq"
+                    //         }
+                    //     ],
+                    //     "condition": {
+                    //         "value": "Diversified Holdings",
+                    //         "operator": "eq"
+                    //     }
+                    // },
                     {
                         "indicatorId": "0-73"
                     },
                     {
-                        "indicatorId": "1-1",
-                        "meta": [
-                            {
-                                "id": 6,
-                                "value": "q1",
-                                "operator": "eq"
-                            },
-                            {
-                                "id": 7,
-                                "value": 2015,
-                                "operator": "eq"
-                            }
-                        ],
-                        "condition": {
-                            "operator": "bt",
-                            "value": 1000000
-                        }
+                        "indicatorId": "1-1",//Revenues
+                        "meta": period
                     },
                     {
-                        "indicatorId": "1-2",
-                        "meta": [
-                            {
-                                "id": 6,
-                                "value": "q1",
-                                "operator": "eq"
-                            },
-                            {
-                                "id": 7,
-                                "value": 2015,
-                                "operator": "eq"
-                            }
-                        ],
-                        "condition": {
-                            "operator": "bt",
-                            "value": 1000000
-                        }
+                        "indicatorId": "1-2",//Cost of Goods Sold
+                        "meta": period
                     },
                     {
-                        "indicatorId": "1-49",
-                        "meta": [
-                            {
-                                "id": 6,
-                                "value": "q1",
-                                "operator": "eq"
-                            },
-                            {
-                                "id": 7,
-                                "value": 2015,
-                                "operator": "eq"
-                            }
-                        ],
-                        "condition": {
-                            "operator": "bt",
-                            "value": 1000000
-                        }
+                        "indicatorId": "1-12",//Selling, General and Administrative
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "1-49",//Income from Continuing Operations
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "2-5",//Receivables, net
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "2-21",//Total Current Assets
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "2-22",//Property, Plant and Equipment, net
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "2-41",//Total Assets
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "2-57",//Total Current Liabilities
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "4-6",//Total Debt
+                        "meta": period
                     },
 
+                    {
+                        "indicatorId": "3-2",//Depreciation & Amortisation
+                        "meta": period
+                    },
+                    {
+                        "indicatorId": "3-13",//Operating Cash Flow
+                        "meta": period
+                    }
                 ]
             })
         ).then(function (response) {
